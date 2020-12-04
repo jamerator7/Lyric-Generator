0f0a70e8f7b6e16f7d0a2efdda435b7fc4008a03
james_reppas <jamerator1@gmail.com>
2020-12-04 10:41:10 -0800

    Finished

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # My project will ask the user for a song and then give them the lyrics to the song.
 import lyricsgenius
+genius = lyricsgenius.Genius("0wYb-xsVfpkx1CtxOyk_-FfAMqAiIXhYLHg8blCZ3qvmrpJgOwV1ycvQSgM_1Kb4")
 
 
-def lyrics(artist, artist1, genius):
+def lyrics(artist, artist1):
     print(" ")
     print("What", artist1, "song do you want to know the lyrics to? ")
     title = str(input(""))
@@ -13,18 +14,18 @@ def lyrics(artist, artist1, genius):
         print(" ")
     except AttributeError:
         return print("No song found")
-    loop(artist, artist1, genius)
+    loop(artist, artist1)
 
 
-def loop(artist, artist1, genius):
-    print("Done. Do you want to know a new artist or know more", artist1, "(y/n)")
+def loop(artist, artist1):
+    print("Done. Do you want a new artist (y) or know more", artist1, "(n)?")
     decide = input("")
     if decide == "y":
         print(" ")
         main()
     else:
         print(" ")
-        lyrics(artist, artist1, genius)
+        lyrics(artist, artist1)
 
 
 def main():
@@ -33,15 +34,15 @@ def main():
     artist1 = str(input("Who is your favorite musical artist? "))
     song_finder[0] = artist1
     song_finder[2] = input("Do you want to know some of their songs? (y/n) ")
-    genius = lyricsgenius.Genius("0wYb-xsVfpkx1CtxOyk_-FfAMqAiIXhYLHg8blCZ3qvmrpJgOwV1ycvQSgM_1Kb4")
     if song_finder[2] == "y":
         print("How many", song_finder[0], "songs do you wanna know?")
         song_finder[1] = int(input(""))
     try:
         artist = genius.search_artist(song_finder[0], max_songs=song_finder[1], sort="popularity")
+        lyrics(artist, artist1)
     except AttributeError:
-        return print("ERROR No Artist Found")
-    lyrics(artist, artist1, genius)
+        print("ERROR No Artist Found")
+        main()
 
 
 if __name__ == '__main__':
